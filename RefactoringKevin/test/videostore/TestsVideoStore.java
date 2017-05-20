77382f956f67920f971353f9084c45c751bd10c7
kevberja <kevirgitano@gmail.com>
2017-05-20 13:09:28 +0200

    Making more tests

--- RefactoringKevin/test/videostore/TestsVideoStore.java
+++ RefactoringKevin/test/videostore/TestsVideoStore.java
@@ -9,13 +9,13 @@ public class TestsVideoStore {
 
 		// Movies
 		private Movie fastandfurious;
-		private Movie americanpie;
+		private Movie up;
 		private Movie titanic;
 
 		// Rentals
 
 		private Rental rFastandfurious;
-		private Rental rAmericanpie;
+		private Rental rUp;
 		private Rental rTitanic;
 
 		// Customers
@@ -26,17 +26,17 @@ public class TestsVideoStore {
 
 		public void setUp() {
 
-			// Set Up Movies
-			fastandfurious = new Movie("Fast and Furious 8", Movie.CHILDRENS);
-			americanpie = new Movie("American Pie 6", Movie.REGULAR);
-			titanic = new Movie("Titanic", Movie.NEW_RELEASE);
+			//Movies
+			fastandfurious = new Movie("Fast and Furious 8", Movie.NEW_RELEASE);
+			up = new Movie("Up", Movie.CHILDRENS);
+			titanic = new Movie("Titanic", Movie.REGULAR);
 
-			// Set Up Rentals
-			rFastandfurious = new Rental(fastandfurious, 7);
-			rAmericanpie = new Rental(americanpie, 3);
-			rTitanic = new Rental(titanic, 5);
+			//Rentals
+			rFastandfurious = new Rental(fastandfurious, 4);
+			rUp = new Rental(up, 7);
+			rTitanic = new Rental(titanic, 10);
 
-			// Set Up Customers
+			//Customers
 			david = new Customer("David");
 			alberto = new Customer("Alberto");
 		}
@@ -44,11 +44,66 @@ public class TestsVideoStore {
 		@Test
 		public void testGetTitleMovie() {
 			String movieTitle = "";
-		
 			Movie movie = this.fastandfurious;
 			movieTitle = "Fast and Furious 8";
 			
-			assertEquals(this.fastandfurious.getTitle(), movieTitle);
+			assertEquals(movie.getTitle(), movieTitle);
 
 		}
+		
+		@Test
+		public void testGetCustomerName() {
+			String customer = "";
+			Customer cust = this.alberto;
+			customer = "Alberto";
+			
+			assertEquals(cust.getName(), customer);
+		}
+		
+		@Test
+
+		public void testGetRentalMovie() {
+			String movie = "";
+			Rental rental = this.rUp;
+			movie = rental.getMovie().getTitle();
+			
+			assertEquals("Up", movie);
+		}
+		
+		@Test
+		public void testCustomerNoStatement() {
+			String statement = "";
+			Customer customer = this.david;
+			statement = customer.statement();
+			
+			assertEquals("Rental Record for David\nAmount owed is 0.0\nYou earned 0 frequent renter points", statement);
+		}
+		
+		@Test
+		public void testAddRentalToCustomer() {
+			String rental = "";
+			this.david.addRental(rUp);
+			rental = this.david.statement();
+
+			assertEquals("Rental Record for David\n\tUp\t7.5\nAmount owed is 7.5\nYou earned 1 frequent renter points", rental);
+		}
+		
+		@Test
+		public void testCustomersStatement() {
+			String statement = "";
+			Customer[] customers = new Customer[2];
+			customers[0] = this.david;
+			customers[1] = this.alberto;
+			
+			david.addRental(rFastandfurious);
+			david.addRental(rTitanic);
+			alberto.addRental(rUp);
+			
+			for (int i = 0; i < customers.length; i++) {
+				statement += customers[i].statement();
+				if (i < customers.length - 1)
+					statement += "\n\n";
+			}
+			assertEquals("Rental Record for David\n\tFast and Furious 8\t12.0\n\tTitanic\t14.0\nAmount owed is 26.0\nYou earned 3 frequent renter points\n\nRental Record for Alberto\n\tUp\t7.5\nAmount owed is 7.5\nYou earned 1 frequent renter points", statement);
+		}	
 }
